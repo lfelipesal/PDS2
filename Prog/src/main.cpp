cd3ce5910e7de76a0aab4118bdc83171db24f4d3
luiz <luiz49138@gmail.com>
2022-04-17 13:53:37 -0300

    passando a pasta das att

---
+++ Prog/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "oi Easy C++ project!" << std::endl;
+    std::cout << "project!" << std::endl;
+    std::cout << "project!" << std::endl;
+    std::cout << "project!" << std::endl;
+}
